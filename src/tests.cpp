a5f3e8d0a90607a16708f273d5c650bdaf25c2b1
JonathanHallstrom <84847878+JonathanHallstrom@users.noreply.github.com>
2022-04-30 16:08:36 +0200

    add header to include all headers

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,13 +1,7 @@
-#include "lmj_io/lmj_io.hpp"
-#include "lmj_math/lmj_math.hpp"
-#include "lmj_containers/lmj_containers.hpp"
-#include "lmj_utils/lmj_utils.hpp"
+#include "lmj_include_all.hpp"
 
 #include <limits>
-#include <iostream>
 #include <cmath>
-#include <iomanip>
-#include <map>
 
 int main() {
     {
