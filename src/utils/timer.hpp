c7ceb338153dff5eeb0b9432c9da8aa4742ebbc6
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-03-08 22:38:51 +0100

    add pretty printing to timer

--- src/utils/timer.hpp
+++ src/utils/timer.hpp
@@ -25,9 +25,44 @@ struct timer {
         return curr_time();
     }
 
+    [[nodiscard]] std::string elapsed_pretty() const {
+        const auto now = high_resolution_clock::now();
+        const auto dur = duration_cast<nanoseconds>(now - start_time);
+        const auto elapsed_nanoseconds = dur.count();
+        const auto elapsed_microseconds = elapsed_nanoseconds / 1000.0;
+        const auto elapsed_milliseconds = elapsed_microseconds / 1000.0;
+        const auto elapsed_seconds = elapsed_milliseconds / 1000.0;
+        const auto elapsed_minutes = elapsed_seconds / 60.0;
+        const auto elapsed_hours = elapsed_minutes / 60.0;
+        const auto elapsed_days = elapsed_hours / 24.0;
+        const auto elapsed_weeks = elapsed_days / 7.0;
+        auto in_range = [](auto x) {
+            return 1.0 <= x && x <= 1000.0;
+        };
+        if (in_range(elapsed_nanoseconds))
+            return std::to_string(elapsed_nanoseconds) + "ns";
+        if (in_range(elapsed_microseconds))
+            return std::to_string(elapsed_microseconds) + "us";
+        if (in_range(elapsed_milliseconds))
+            return std::to_string(elapsed_milliseconds) + "ms";
+        if (in_range(elapsed_seconds))
+            return std::to_string(elapsed_seconds) + "s";
+        if (in_range(elapsed_minutes))
+            return std::to_string(elapsed_minutes) + "m";
+        if (in_range(elapsed_hours))
+            return std::to_string(elapsed_hours) + "h";
+        if (in_range(elapsed_days))
+            return std::to_string(elapsed_days) + "d";
+        return std::to_string(elapsed_weeks) + "w";
+    }
+
     ~timer() {
         if (print) {
-            std::cerr << curr_time() << "s\n";
+            std::cout.flush();
+            std::cerr.flush();
+            fflush(nullptr);
+            std::cerr << (elapsed_pretty() + "\n");
+
         }
     }
 };
