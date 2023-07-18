fa75a6c345fc7d5447b8768a507b84e116e739f0
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-07-18 11:50:42 +0200

    remove unnecessary static_vector_iterator implementations

--- src/containers/static_vector.hpp
+++ src/containers/static_vector.hpp
@@ -4,8 +4,10 @@
 #include <array>
 #include <cassert>
 #include <cstdint>
+#include <iterator>
 #include <limits>
 #include <numeric>
+#include <type_traits>
 #include <vector>
 
 namespace lmj {
@@ -26,189 +28,20 @@ constexpr auto all_same_types() {
 }
 
 template<class T, std::size_t _capacity>
-class static_vector;
-
-template<class T, std::size_t _capacity>
-class static_vector_iterator {
-public:
-    using size_type = decltype(detail::needed_uint<_capacity>());
-    using difference_type = std::make_signed_t<decltype(detail::needed_uint<
-            _capacity < std::numeric_limits<std::uint64_t>::max() / 2
-            ? _capacity * 2 + 1
-            : std::numeric_limits<std::uint64_t>::max()>())>;
-    using value_type = T;
-    using reference = value_type &;
-    using iterator_category = std::random_access_iterator_tag;
-
-private:
-    friend static_vector<T, _capacity>;
-
-    value_type *m_ptr;
-
-public:
-    constexpr static_vector_iterator() : m_ptr{nullptr} {}
-
-    template<class G>
-    constexpr explicit static_vector_iterator(G ptr) : m_ptr{ptr} {}
-
-    constexpr reference operator*() const noexcept {
-        return *m_ptr;
-    }
-
-    constexpr static_vector_iterator &operator++() {
-        ++m_ptr;
-        return *this;
-    }
-
-    constexpr static_vector_iterator operator++(int) {
-        return static_vector_iterator{m_ptr++};
-    }
-
-    constexpr static_vector_iterator &operator--() {
-        --m_ptr;
-        return *this;
-    }
-
-    constexpr static_vector_iterator operator--(int) {
-        return static_vector_iterator{m_ptr--};
-    }
-
-    constexpr static_vector_iterator operator+(difference_type d) const {
-        return static_vector_iterator{m_ptr + d};
-    }
-
-    constexpr static_vector_iterator operator-(difference_type d) const {
-        return static_vector_iterator{m_ptr - d};
-    }
-
-    constexpr difference_type operator-(static_vector_iterator d) const {
-        return static_cast<difference_type>(m_ptr - d.m_ptr);
-    }
-
-    constexpr static_vector_iterator &operator+=(difference_type d) {
-        m_ptr += d;
-        return *this;
-    }
-
-    constexpr static_vector_iterator &operator-=(difference_type d) {
-        m_ptr -= d;
-        return *this;
-    }
-
-    constexpr reference operator[](size_type idx) const {
-        return m_ptr[idx];
-    }
-
-    [[nodiscard]] constexpr std::strong_ordering operator<=>(static_vector_iterator const &) const noexcept = default;
-
-    [[nodiscard]] constexpr bool operator==(static_vector_iterator const &) const noexcept = default;
-};
-
-template<class T, std::size_t _capacity>
-class static_vector_const_iterator {
-public:
-    using size_type = decltype(detail::needed_uint<_capacity>());
-    using difference_type = std::make_signed_t<decltype(detail::needed_uint<
-            _capacity < std::numeric_limits<std::uint64_t>::max() / 2
-            ? _capacity * 2 + 1
-            : std::numeric_limits<std::uint64_t>::max()>())>;
-    using value_type = const T;
-    using reference = value_type &;
-    using iterator_category = std::random_access_iterator_tag;
-
-private:
-    friend static_vector<T, _capacity>;
-
-    value_type *m_ptr;
-
-public:
-    constexpr static_vector_const_iterator() : m_ptr{nullptr} {}
-
-    constexpr static_vector_const_iterator(static_vector_iterator<T, _capacity> i) : m_ptr{&*i} {}
-
-    template<class G>
-    constexpr explicit static_vector_const_iterator(G *ptr) : m_ptr{ptr} {}
-
-    constexpr reference operator*() const noexcept {
-        return *m_ptr;
-    }
-
-    constexpr static_vector_const_iterator &operator++() {
-        ++m_ptr;
-        return *this;
-    }
-
-    constexpr static_vector_const_iterator operator++(int) {
-        return static_vector_const_iterator{m_ptr++};
-    }
-
-    constexpr static_vector_const_iterator &operator--() {
-        --m_ptr;
-        return *this;
-    }
-
-    constexpr static_vector_const_iterator operator--(int) {
-        return static_vector_const_iterator{m_ptr--};
-    }
-
-    constexpr static_vector_const_iterator operator+(difference_type d) const {
-        return static_vector_const_iterator{m_ptr + d};
-    }
-
-    constexpr static_vector_const_iterator operator-(difference_type d) const {
-        return static_vector_const_iterator{m_ptr - d};
-    }
-
-    constexpr difference_type operator-(static_vector_const_iterator d) const {
-        return static_cast<difference_type>(m_ptr - d.m_ptr);
-    }
-
-    constexpr static_vector_const_iterator &operator+=(difference_type d) {
-        m_ptr += d;
-        return *this;
-    }
-
-    constexpr static_vector_const_iterator &operator-=(difference_type d) {
-        m_ptr -= d;
-        return *this;
-    }
-
-    constexpr reference operator[](size_type idx) const {
-        return m_ptr[idx];
-    }
-
-    [[nodiscard]] constexpr std::strong_ordering
-    operator<=>(static_vector_const_iterator const &) const noexcept = default;
-
-    [[nodiscard]] constexpr bool operator==(static_vector_const_iterator const &) const noexcept = default;
-};
-
-template<class T, std::size_t _capacity>
-constexpr static_vector_iterator<T, _capacity>
-operator+(const typename static_vector_iterator<T, _capacity>::difference_type diff,
-          static_vector_iterator<T, _capacity> const &iter) noexcept { return iter + diff; }
-
-template<class T, std::size_t _capacity>
-constexpr static_vector_const_iterator<T, _capacity>
-operator+(const typename static_vector_const_iterator<T, _capacity>::difference_type diff,
-          static_vector_const_iterator<T, _capacity> const &iter) noexcept { return iter + diff; }
-
-static_assert(std::random_access_iterator<static_vector_iterator<int, 1>>);
-static_assert(std::random_access_iterator<static_vector_const_iterator<int, 1>>);
-
-template<class T, std::size_t _capacity>
 class static_vector {
 private:
-    friend static_vector_iterator<T, _capacity>;
-
 public:
-    using iterator = static_vector_iterator<T, _capacity>;
-    using const_iterator = static_vector_const_iterator<T, _capacity>;
-    using size_type = iterator::size_type;
-    using difference_type = iterator::difference_type;
     using value_type = T;
-    using reference = T &;
-    using const_reference = T const &;
+    using reference = value_type &;
+    using const_reference = value_type const &;
+    using pointer = value_type *;
+    using const_pointer = const value_type *;
+    using iterator = pointer;
+    using size_type = std::size_t;
+    using difference_type = std::make_signed_t<size_type>;
+    using const_iterator = const_pointer;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
     value_type m_data[_capacity]{};
     size_type m_size{};
@@ -354,7 +187,7 @@ public:
 #if __has_builtin(__builtin_unreachable)
             __builtin_unreachable()
 #endif
-            ;
+                    ;
 
         while (i2 != end())
             *i1++ = *i2++;
@@ -395,27 +228,27 @@ public:
         return begin() + m_size;
     }
 
-    [[nodiscard]] constexpr std::reverse_iterator<const_iterator> rbegin() const {
+    [[nodiscard]] constexpr const_reverse_iterator rbegin() const {
         return std::reverse_iterator{end()};
     }
 
-    [[nodiscard]] constexpr std::reverse_iterator<const_iterator> rend() const {
+    [[nodiscard]] constexpr const_reverse_iterator rend() const {
         return std::reverse_iterator{begin()};
     }
 
-    [[nodiscard]] constexpr std::reverse_iterator<iterator> rbegin() {
+    [[nodiscard]] constexpr reverse_iterator rbegin() {
         return std::reverse_iterator{end()};
     }
 
-    [[nodiscard]] constexpr std::reverse_iterator<iterator> rend() {
+    [[nodiscard]] constexpr reverse_iterator rend() {
         return std::reverse_iterator{begin()};
     }
 
-    [[nodiscard]] constexpr std::reverse_iterator<const_iterator> crbegin() const {
+    [[nodiscard]] constexpr const_reverse_iterator crbegin() const {
         return std::reverse_iterator{cend()};
     }
 
-    [[nodiscard]] constexpr std::reverse_iterator<const_iterator> crend() const {
+    [[nodiscard]] constexpr const_reverse_iterator crend() const {
         return std::reverse_iterator{cbegin()};
     }
 
@@ -429,8 +262,8 @@ public:
         return true;
     }
 
-    template<class G>
-    [[nodiscard]] constexpr bool operator!=(G const &other) const {
+    template<std::size_t other_capacity>
+    [[nodiscard]] constexpr bool operator!=(static_vector<T, other_capacity> const &other) const {
         if (m_size != other.m_size)
             return true;
         for (size_type i = 0; i < m_size; ++i)
